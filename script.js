7a8e7180c01a173851e97a9547fa6669df83ec34
Shelbie Fecher <63127198+Sfecher@users.noreply.github.com>
2021-02-09 08:11:11 -0500

    US5 script

---
+++ script.js
@@ -0,0 +1,31 @@
+//issue #9 about poor design
+//just setting up the layout of how the links will be displayed.
+function hamburgerBar() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function clickFunctionFood () {
+};
+
+
+function showResult(str) {
+  if (str.length==0) {
+    document.getElementById("livesearch").innerHTML="";
+    document.getElementById("livesearch").style.border="0px";
+    return;
+  }
+  var xmlhttp=new XMLHttpRequest();
+  xmlhttp.onreadystatechange=function() {
+    if (this.readyState==4 && this.status==200) {
+      document.getElementById("livesearch").innerHTML=this.responseText;
+      document.getElementById("livesearch").style.border="1px solid #A5ACB2";
+    }
+  }
+  xmlhttp.open("GET","livesearch.php?q="+str,true);
+  xmlhttp.send();
+}
